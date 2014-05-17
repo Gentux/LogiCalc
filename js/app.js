8a21f294c10524234f7e111efed8cf43dec62850
Romain Soufflet <gentux@gentux-desktop.home.wa-pgm.com>
2014-05-17 13:42:15 +0200

    Add first ractive test.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,33 @@
 define([
+  'ractive',
 ],
-function () {
+function (Ractive) {
   'use strict';
 
   function init() {
+    var data = {
+      numbers: [
+        {
+          uid: 0,
+          value: 0,
+          name: 'Variable',
+          editable: false
+        }
+      ]
+    };
+    var ractive = new Ractive({
+      el: '.logicalc-app',
+        template: '#tmpl',
+        data: data
+    });
+    ractive.on({
+      editable: function (event) {
+        event.context.editable = (event.context.editable === true) ? false : true;
+        this.set(event.keypath, event.context);
+        console.log(this);
+        console.log(event);
+      }
+    });
   }
 
   return {init: init};
